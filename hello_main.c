c13692f9442a5aad9f663552a2aafb9b8762a262
funEE361 <dmclaugh@umass.edu>
2021-12-22 08:16:14 -0500

    First commit of the hello_world project

---
+++ hello_main.c
@@ -0,0 +1,15 @@
+/****************************************************************
+hello_main.c -- this program prints Hello World! to the screen. 
+
+written for a laptop with standard I/O rather than an embedded MCU
+
+Date        Author          Revision
+12/20/21    D. McLaughlin   initial code write
+*******************************************************************/
+
+#include <stdio.h>      // we need this to user the printf function
+
+int main(void){
+    printf("Hello World!\n");
+    return 0;
+}
---
+++ makefile
@@ -0,0 +1,6 @@
+CC=gcc
+letsgo: hello_main.c 
+	$(CC) -o letsgo hello_main.c
+# end of file
+# See the tutorial here:
+# https://cs.colby.edu/maxwell/courses/tutorials/maketutor/
